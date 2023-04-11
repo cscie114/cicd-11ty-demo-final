33572043eb5a22dabb33f19f9b1df22867a075e7
Michael D. Hilborn <mhilborn@biggles2k.com>
2023-04-11 11:45:07 -0400

    Initial commit of basic code base.

---
+++ src/_data/movie.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const axios = require('axios').default;
+const movieId = "tt0090605";
+
+module.exports = async function() {
+
+    try {
+        const movie =
+            await axios.get("https://omdbapi.com/?i=" + movieId + "&apiKey=" + process.env.MOVIE_API_KEY);
+        return movie.data;
+
+    } catch (error) {
+        console.error(error);
+
+    }
+
+};
